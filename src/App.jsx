cf9814cf5a928b75b78bd42465b1cd97bd488459
Nico Picchio <nicolapicchio@gmail.com>
2022-04-12 17:29:01 +0100

    exercise set up and form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css';
 
 export default function App() {
-    return (
-        <div className="App">
-
-        </div>
-    );
+	return (
+		<div id='container'>
+			<h1>User login</h1>
+			<form>
+				<input
+					type='text'
+					id='username'
+					name='username'
+					placeholder='Username'
+				/>
+				<br />
+				<input
+					type='password'
+					id='username'
+					name='username'
+					placeholder='Password'
+				/>
+				<br />
+				<input type='submit' value='Login' />
+			</form>
+		</div>
+	);
 }
