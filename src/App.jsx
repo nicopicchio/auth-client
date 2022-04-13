0000043557a04d8d604726d3dd72936c4632aa45
Nico Picchio <nicolapicchio@gmail.com>
2022-04-13 18:14:18 +0100

    requirement 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
 
+const initialBodyState = {
+	username: '',
+	password: '',
+};
+
 export default function App() {
-	console.log('rendering app')
-	const [body, setBody] = useState({
-		username: '',
-		password: '',
-	});
+	const [body, setBody] = useState(initialBodyState);
+	const [newUser, setNewUser] = useState();
+	const [isLogin, setIsLogin] = useState(false);
 
 	const onInputChange = (e) => {
 		setBody({ ...body, [e.target.name]: e.target.value });
 	};
 
-	const onFormSubmit = (e) => {
-		e.preventDefault();
+	const loadLoginForm = () => {
+		setIsLogin(!isLogin);
+	};
+
+	const registerFetch = () => {
 		fetch('http://localhost:4000/register', {
 			method: 'POST',
 			headers: {
@@ -23,35 +29,80 @@ export default function App() {
 			body: JSON.stringify(body),
 		})
 			.then((res) => res.json())
-			.then((jsonResponse) => console.log(jsonResponse));
+			.then((jsonResponse) =>
+				setNewUser(`Welcome onboard, ${jsonResponse.registeredUser.username}`)
+			)
+			.then(setBody(initialBodyState))
+			.catch((err) => {
+				setNewUser('Something went wrong!');
+			});
+	};
+
+	const loginFetch = () => {
+		fetch('http://localhost:4000/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(body),
+		})
+			.then((res) => res.json())
+			.then((jsonResponse) => {
+				if (jsonResponse === 'Access denied!') {
+					setNewUser('Invalid username or password!')
+				} else
+				setNewUser(`User token is: ${jsonResponse.token}`)
+			})
+			.then(setBody(initialBodyState))
+			.catch((err) => {
+				setNewUser('Something went wrong!');
+			});
+	}
+
+	const onFormSubmit = (e) => {
+		e.preventDefault();
+		if (!isLogin) {
+			registerFetch()
+		} else
+		loginFetch()
 	};
 
 	return (
-		<div id='container'>
-			<h1>Register</h1>
-			<form onSubmit={onFormSubmit}>
-				<input
-					type='text'
-					id='username'
-					name='username'
-					placeholder='Username'
-					value={body.username}
-					onChange={onInputChange}
-				/>
-				<br />
-				<input
-					type='password'
-					id='password'
-					name='password'
-					placeholder='Password'
-					value={body.password}
-					onChange={onInputChange}
-				/>
-				<br />
-				<input type='submit' value='Sign up!' />
-			</form>
-			<p>Already registered?</p>
-			<p className='login-link'>Login</p>
-		</div>
+		<>
+			<div id='container'>
+				<h1>
+					{!isLogin && 'Register'}
+					{isLogin && 'Login'}
+				</h1>
+				<form onSubmit={onFormSubmit}>
+					<input
+						type='text'
+						id='username'
+						name='username'
+						placeholder='Username'
+						value={body.username}
+						onChange={onInputChange}
+						required
+					/>
+					<br />
+					<input
+						type='password'
+						id='password'
+						name='password'
+						placeholder='Password'
+						value={body.password}
+						onChange={onInputChange}
+						required
+					/>
+					<br />
+					<input type='submit' />
+				</form>
+				<p>Already registered?</p>
+				<p className='login-link' onClick={loadLoginForm}>
+					Login
+				</p>
+			</div>
+			<div>{newUser && <p className='registration-message'>{newUser}</p>}</div>
+		</>
 	);
 }
